b23aa58dae683d982d7462fec9043131db121ca5
adamandp <adamanandaputra@gmail.com>
2025-12-12 19:25:12 +0700

    chore: add spacing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { Button } from "./components/ui/button";
-
 function App() {
   return (
     <div className="general">
-      <p className="font-GeneralSans-Extralight">hello</p>
+      <p className="H1Bold">hello</p>
       <p className="font-GeneralSans-">hello</p>
       <p>hello</p>
     </div>
